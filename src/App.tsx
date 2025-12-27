75bcf06a80ee67c94e9b8f05a455defcb4b69429
Ravindra Jadhav <jadhaor181@gmail.com>
2025-12-27 14:55:31 +0530

    Phase 1 - Project Setup with Tauri & React Desktop

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App(){
+
+  return(
+    <div style={{padding: 20}}>
+    <h1>Wispr Flow Clone</h1>
+    <p>This is a clone of the Wispr Flow application.</p>
+    </div>
+  );
+}
+export default App;
